50f507043379c0561f1a745f24212fb497c9642d
unknown <frigidor@bluewin.ch>
2018-05-20 09:58:27 +0200

    Absolute URLs

--- src/Config/dist.php
+++ src/Config/dist.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
+    'baseUrl' => '',
+
     'apiEndpointUserLogs' => '',
     'apiParametersUserLogs' => [
         'token' => '',
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,18 +3,12 @@
 namespace Controller;
 
 use Core\InputParameters;
+use Helper\ConfigHelper;
 
 class HomeController implements ControllerInterface {
     public function run(): array {
-        $username = InputParameters::get('username');
-        $fromDate = InputParameters::get('fromDate');
-        $toDate = InputParameters::get('toDate');
-
-        if($username && $fromDate && $toDate) {
-            header('Location: /' . urlencode($username) . '/' . $fromDate . '/' . $toDate);
-            exit();
-        }
-
-        return [];
+        return [
+            'config' => ConfigHelper::getConfig()
+        ];
     }
 }
